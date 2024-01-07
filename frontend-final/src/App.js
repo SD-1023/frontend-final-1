89ac7dd8b3ce9d2168ee0408753a34c6962e3e40
cesarNeZe <cesarzaitoun@gmail.com>
2024-01-07 23:14:43 +0200

    add basename

--- frontend-final/src/App.js
+++ frontend-final/src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import { Router } from 'express';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='/frontend-final-1'>
+    <h1>Hello Welcome to our final project</h1>
+    <p>Done by: Ahmad Masri, Abdullah Bahrawi, Ahmad Breeka, Cesar Zaitoun, Fadi Halteh</p>
+    </BrowserRouter>
   );
 }
 
