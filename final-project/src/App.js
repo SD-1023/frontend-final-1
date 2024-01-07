406955e3798375c788669af2e2e18eeb2e326bfa
cesarNeZe <cesarzaitoun@gmail.com>
2024-01-07 23:40:31 +0200

    mock site

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
    <BrowserRouter basename='/frontend-final-1'>
+    <h1>Final Project </h1>
+    <p>Done by : Ahmand Masri, Cesar Zaitoun</p>
    </BrowserRouter>
   );
 }
