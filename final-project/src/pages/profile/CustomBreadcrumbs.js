bfa440e605aa44dd3ffbb49e068f618f1695cf48
temo-sudo <you@example.com>
2024-01-31 00:10:59 +0200

    Fixing an issue in the profile breadcrumbs component

--- final-project/src/pages/profile/CustomBreadcrumbs.js
+++ final-project/src/pages/profile/CustomBreadcrumbs.js
@@ -8,7 +8,7 @@ export const CustomBreadcrumbs = ({ items, setCurrentItem }) => {
 
     const onClickHandler = (item) => {
         if (item.path) {
-            navigate(item.path);
+            return navigate('/');
         } else {
             setCurrentItem(item.navTitle);
         }
@@ -17,7 +17,9 @@ export const CustomBreadcrumbs = ({ items, setCurrentItem }) => {
     return <Breadcrumbs sx={{ display: { xs: 'none', sm: 'block' } }} separator="›" aria-label="breadcrumb">
 
         {items.map((item, i) => (
-            <Link component={RouterLink} underline="hover" key={i} onClick={() => onClickHandler(item)}
+            <Link component={RouterLink} underline="hover" key={i}
+             to={item.path}
+             onClick={() => onClickHandler(item)}
                 sx={{ color: item.color ? item.color : '#626262', fontSize: '16px' }}>
                 {item.title}
             </Link>
